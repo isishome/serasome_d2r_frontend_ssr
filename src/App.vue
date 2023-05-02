b5146d3371257b2f3152dcde17b15da455679e3c
DESKTOP-BUKLG6N\User <sjcho@morai.ai>
2023-05-02 15:23:28 +0900

    update dark flicker

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <script setup>
-import { onBeforeMount } from 'vue'
+import { ref, onMounted } from 'vue'
 import { useQuasar } from 'quasar'
 
 const $q = useQuasar()
 const darkName = import.meta.env.VITE_APP_D2R_DARK_NAME
 const isDark = $q.cookies.has(darkName) ? $q.cookies.get(darkName).toString() === 'true' : true
+const view = ref(false)
+$q.dark.set(isDark)
 
-onBeforeMount(() => {
-  $q.dark.set(isDark)
+onMounted(() => {
+  view.value = true
 })
 </script>
 <template>
-  <router-view />
+  <router-view v-if="view" />
 </template>
